9784483f0e46e6dd1eb0b48bd23c2ffb29b1b15a
Muhammad Nouman Imrn <noman.imran047@gmail.com>
2020-02-22 15:52:37 -0800

    first commit

---
+++ config/scart.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'version' => '3.4.3',
+    'homepage' => '',
+    'name' => 'JamGo',
+    'title' => ' eCommerce for Business',
+    'github' => '',
+    'email' => 'lanhktc@gmail.com',
+    'settings' => [
+    ],
+];
